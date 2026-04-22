4491d3adcbed5f5719407ca6b1526145689b12d3
Lucero <luceromarqueziphone@gmail.com>
2026-04-22 11:49:55 -0400

    Correcion en Dto/Producto

--- TiendaAccesorios/DTO/Producto/AgregarProducto/AgregarProductoInput.cs
+++ TiendaAccesorios/DTO/Producto/AgregarProducto/AgregarProductoInput.cs
@@ -7,11 +7,11 @@ public class AgregarProductoInput
 {
     public required string NombreProducto { get; set; }
     public string? Descripcion { get; set; }
-    public string? Marca { get; set; }
-    public string? Color { get; set; }
+    public required string Marca { get; set; }
+    public required string Color { get; set; }
     public decimal Precio { get; set; }
     public int Stock { get; set; }
-  
+
 
     public Guid IdCategoria { get; set; }
     public Guid IdProveedor { get; set; }
