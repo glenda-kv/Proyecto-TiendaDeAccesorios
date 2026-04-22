0ea0a614ea0a94fe5b94f05781b7240bdec2007b
Lucero <luceromarqueziphone@gmail.com>
2026-04-22 10:35:59 -0400

    Agregué el codigo en el DTO de la venta

--- TiendaAccesorios/DTO/Venta/GenerarVentaInput.cs
+++ TiendaAccesorios/DTO/Venta/GenerarVentaInput.cs
@@ -4,5 +4,20 @@ namespace TiendaAccesorios.DTO.Venta;
 
 public class GenerarVentaInput
 {
+    public Guid IdCliente { get; set; }
+    public required string ModalidadPago { get; set; }
 
+    public List<ProductosDeEntrada> Detalle { get; set; } = new List<ProductosDeEntrada>();
 }
+
+public class ProductosDeEntrada
+{
+    public required string NombreProducto { get; set; }
+    public required string Marca { get; set; }
+    public required string Color { get; set; }
+    public int Cantidad { get; set; }
+}
+
+
+
+
