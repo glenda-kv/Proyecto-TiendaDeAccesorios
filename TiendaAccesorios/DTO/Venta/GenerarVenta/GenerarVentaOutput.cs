1c6486e0be5654d898e4ac69f045855f47ecdddb
Lucero <luceromarqueziphone@gmail.com>
2026-04-26 18:58:46 -0400

    Coreccion en  DTO/Venta

--- TiendaAccesorios/DTO/Venta/GenerarVenta/GenerarVentaOutput.cs
+++ TiendaAccesorios/DTO/Venta/GenerarVenta/GenerarVentaOutput.cs
@@ -6,19 +6,19 @@ public class GenerarVentaOutput
 {
     public Guid IdVenta { get; set; }
     public DateTime FechaVenta { get; set; }
-    public required string FormaDePago { get; set; }
+    public string FormaDePago { get; set; }
     public decimal Total { get; set; }
     public Guid IdCliente { get; set; }
-    public required string NombreCliente { get; set; }
+    public string NombreCliente { get; set; }
 
     public List<DetalleVentaSalida> Detalles { get; set; } = new();
 }
 
 public class DetalleVentaSalida
 {
-    public required string NombreProducto { get; set; }
-    public required string Marca { get; set; }
-    public required string Color { get; set; }
+    public string NombreProducto { get; set; }
+    public string Marca { get; set; }
+    public string Color { get; set; }
     public int Cantidad { get; set; }
     public decimal PrecioUnitario { get; set; }
     public decimal Subtotal { get; set; }
--- TiendaAccesorios/DTO/Venta/ObtenerVenta/ObtenerVentaOutput.cs
+++ TiendaAccesorios/DTO/Venta/ObtenerVenta/ObtenerVentaOutput.cs
@@ -1,8 +1,18 @@
 using System;
+using TiendaAccesorios.DTO.Venta.GenerarVenta;
 
 namespace TiendaAccesorios.DTO.Venta.ObtenerVenta;
 
 public class ObtenerVentaOutput
 {
+    public Guid IdVenta { get; set; }
+    public DateTime FechaVenta { get; set; }
+    public string FormaDePago { get; set; }
+    public decimal Total { get; set; }
+    public Guid IdCliente { get; set; }
+    public string NombreCliente { get; set; }
+
+    public List<DetalleVentaSalida> Detalles { get; set; } = new();
 
 }
+
