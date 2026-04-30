4a704b0fb8cf02b485831703961a6859c316b92a
Lucero <luceromarqueziphone@gmail.com>
2026-04-30 13:03:50 -0400

    Actualice dto/Producto/acoutput

--- TiendaAccesorios/DTO/Producto/ActualizarProducto/ActualizarProductoOutput.cs
+++ TiendaAccesorios/DTO/Producto/ActualizarProducto/ActualizarProductoOutput.cs
@@ -5,11 +5,18 @@ namespace TiendaAccesorios.DTO.Producto.ActualizarProducto;
 public class ActualizarProductoOutput
 {
     public Guid IdProducto { get; set; }
-    public string NombreProducto { get; set; }
+
+    public required string NombreProducto { get; set; }
+
     public string? Descripcion { get; set; }
-    public string Marca { get; set; }
-    public string Color { get; set; }
+
+    public required string Marca { get; set; }
+
+    public required string Color { get; set; }
+
     public decimal Precio { get; set; }
+
     public int Stock { get; set; }
-    public string NombreCategoria { get; set; }
+
+    public required string NombreCategoria { get; set; }
 }
