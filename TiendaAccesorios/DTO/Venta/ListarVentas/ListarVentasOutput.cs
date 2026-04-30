8cfe1393d0de04551116147da5c8258c265ad79e
Lucero <luceromarqueziphone@gmail.com>
2026-04-30 13:28:15 -0400

    Correcion DTO/Venta/Listar y Obtener

--- TiendaAccesorios/DTO/Venta/ListarVentas/ListarVentasOutput.cs
+++ TiendaAccesorios/DTO/Venta/ListarVentas/ListarVentasOutput.cs
@@ -4,11 +4,19 @@ using System.ComponentModel.DataAnnotations;
 namespace TiendaAccesorios.DTO.Venta.ListarVentas;
 
 public class ListarVentasOutput
+
 {
-    /* public Guid IdVenta { get; set; }
+    public Guid IdVenta { get; set; }
+
     public DateTime FechaVenta { get; set; }
-    public string FormaDePago { get; set; }
+
+    public required string MetodoPago { get; set; }
+
     public decimal Total { get; set; }
+
+    public required string EstadoVenta { get; set; }
+
     public Guid IdCliente { get; set; }
-    public string NombreCliente { get; set; } */
+
+    public required string NombreCliente { get; set; }
 }
--- TiendaAccesorios/DTO/Venta/ObtenerVenta/ObtenerVentaOutput.cs
+++ TiendaAccesorios/DTO/Venta/ObtenerVenta/ObtenerVentaOutput.cs
@@ -5,14 +5,23 @@ namespace TiendaAccesorios.DTO.Venta.ObtenerVenta;
 
 public class ObtenerVentaOutput
 {
-    /* public Guid IdVenta { get; set; }
-    public DateTime FechaVenta { get; set; }
-    public string FormaDePago { get; set; }
-    public decimal Total { get; set; }
-    public Guid IdCliente { get; set; }
-    public string NombreCliente { get; set; }
+    public Guid IdVenta { get; set; }
 
-    public List<DetalleVentaSalida> Detalles { get; set; } = new(); */
+    public DateTime FechaVenta { get; set; }
+
+    public required string MetodoPago { get; set; }
+
+    public decimal Total { get; set; }
+
+    public required string EstadoVenta { get; set; }
+
+    public string? Observacion { get; set; }
+
+    public Guid IdCliente { get; set; }
+
+    public required string NombreCliente { get; set; }
+
+    public List<DetalleVentaSalida> Detalles { get; set; } = new();
 
 }
 
