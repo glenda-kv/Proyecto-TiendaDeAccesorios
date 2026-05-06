d3bdfa563b7501860732ff9e75f66895c72bf049
Lucero <luceromarqueziphone@gmail.com>
2026-05-06 02:24:44 -0400

    Conexion con la API

--- Backend/TiendaAccesorios/Program.cs
+++ Backend/TiendaAccesorios/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("FrontendReact", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:3000")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
